b26bb1c1211acba8dd550a1efbcf117af4c5ebcb
Niladri Adhikary <rickyrickado14@gmail.com>
2024-02-02 00:08:34 +0530

    color change

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -187,8 +187,8 @@ function ChatRoom(props) {
         <ReactMic
           record={record}
           onStop={onRecordingComplete}
-          strokeColor="#000000"
-          backgroundColor="#e37034"
+          strokeColor="#fff"
+          backgroundColor="#364050"
         />
 
         <button type="button" onClick={() => setRecord(!record)} className="relative size-11 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
