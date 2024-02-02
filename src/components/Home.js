b6d36a9b3e8c98fd0a7e2156a3f24f9146656d58
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-02-02 21:35:32 +0530

    tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,21 @@ function Home() {
   return (
     <div className="flex flex-col px-10 gap-5">
 
-      <p className="text-center text-2xl mb-4 mt-2 font-semibold">Introducing "BridgeTogether" - Bridging Conversations, Breaking Barriers</p>
+      <p className="text-center text-2xl mb-4 mt-2 font-semibold">Introducing "NeoChat" - Bridging Conversations, Breaking Barriers</p>
 
-      <p className="text-justify text-lg">Welcome to UnitySpeak, where communication knows no bounds. In a world that thrives on connections, we recognize the importance of inclusive communication. Our revolutionary app is designed to break down barriers and foster seamless conversations between differently-abled individuals and those with diverse abilities.</p>
+      <p className="text-justify text-lg">Welcome to NeoChat, where communication knows no bounds. In a world that thrives on connections, we recognize the importance of inclusive communication. Our revolutionary app is designed to break down barriers and foster seamless conversations between differently-abled individuals and those with diverse abilities.</p>
 
-      <p className="text-justify text-base"><span className="text-lg font-semibold">🌐  A Platform for All:</span> At UnitySpeak, inclusivity is at the core of our mission. Our app provides a platform where individuals of all abilities can engage in meaningful conversations, share experiences, and build connections that transcend limitations.</p>
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🌐  A Platform for All:</span> At NeoChat, inclusivity is at the core of our mission. Our app provides a platform where individuals of all abilities can engage in meaningful conversations, share experiences, and build connections that transcend limitations.</p>
 
-      <p className="text-justify text-base"><span className="text-lg font-semibold">🤝  Connecting Hearts and Minds:</span> Communication is the heartbeat of human connection. Whether you communicate through text, voice, or video, UnitySpeak ensures that every interaction is accessible and enjoyable for everyone involved. Our user-friendly interface caters to various communication styles, ensuring a truly inclusive experience.</p>
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🤝  Connecting Hearts and Minds:</span> Communication is the heartbeat of human connection. Whether you communicate through text, voice, or video, NeoChat ensures that every interaction is accessible and enjoyable for everyone involved. Our user-friendly interface caters to various communication styles, ensuring a truly inclusive experience.</p>
 
-      <p className="text-justify text-base"><span className="text-lg font-semibold">🎙️  Voice of Empowerment:</span> For those who face speech challenges, UnitySpeak empowers them with innovative speech-to-text features, making their voices heard effortlessly. Additionally, our app facilitates easy communication through customizable symbols and images, ensuring that no message is left unexpressed.</p>
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🎙️  Voice of Empowerment:</span> For those who face speech challenges, NeoChat empowers them with innovative speech-to-text features, making their voices heard effortlessly. Additionally, our app facilitates easy communication through customizable symbols and images, ensuring that no message is left unexpressed.</p>
 
-      <p className="text-justify text-base"><span className="text-lg font-semibold">🌈  Diverse Abilities, United Conversations:</span> Diversity is our strength, and UnitySpeak celebrates the richness of human experience. With features designed to accommodate various abilities, our app becomes a powerful tool for fostering empathy, understanding, and unity among people from all walks of life.</p>
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🌈  Diverse Abilities, United Conversations:</span> Diversity is our strength, and NeoChat celebrates the richness of human experience. With features designed to accommodate various abilities, our app becomes a powerful tool for fostering empathy, understanding, and unity among people from all walks of life.</p>
 
-      <p className="text-justify text-base"><span className="text-lg font-semibold">🚀  Seamless Integration:</span> UnitySpeak seamlessly integrates into your daily life, offering a wide range of features that adapt to your unique communication needs. From interactive chat rooms to personalized communication boards, we are committed to providing a platform that evolves with you.</p>
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🚀  Seamless Integration:</span> NeoChat seamlessly integrates into your daily life, offering a wide range of features that adapt to your unique communication needs. From interactive chat rooms to personalized communication boards, we are committed to providing a platform that evolves with you.</p>
 
-      <p className="text-justify text-base">Join us on this journey of breaking down communication barriers, one conversation at a time. Download UnitySpeak today and be part of a community where everyone's voice is not just heard but celebrated. Together, let's create a world where communication knows no limits.</p>
+      <p className="text-justify text-base">Join us on this journey of breaking down communication barriers, one conversation at a time. Download NeoChat today and be part of a community where everyone's voice is not just heard but celebrated. Together, let's create a world where communication knows no limits.</p>
 
     </div>
   );
