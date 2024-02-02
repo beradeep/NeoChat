c0468b4051712479ccb98350d902276cfcb156ac
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-02-02 19:08:02 +0530

    ui tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,23 @@
 function Home() {
   return (
-    <div style={{ whiteSpace: "pre-wrap" }}>
-      Introducing "UnitySpeak" - Bridging Conversations, Breaking Barriers <br /><br />
+    <div className="flex flex-col px-10 gap-5">
 
-      Welcome to UnitySpeak, where communication knows no bounds. In a world that thrives on connections, we recognize the importance of inclusive communication. Our revolutionary app is designed to break down barriers and foster seamless conversations between differently-abled individuals and those with diverse abilities. <br /><br />
+      <p className="text-center text-2xl mb-4 mt-2 font-semibold">Introducing "BridgeTogether" - Bridging Conversations, Breaking Barriers</p>
 
-      🌐  A Platform for All: At UnitySpeak, inclusivity is at the core of our mission. Our app provides a platform where individuals of all abilities can engage in meaningful conversations, share experiences, and build connections that transcend limitations.<br /><br />
+      <p className="text-justify text-lg">Welcome to UnitySpeak, where communication knows no bounds. In a world that thrives on connections, we recognize the importance of inclusive communication. Our revolutionary app is designed to break down barriers and foster seamless conversations between differently-abled individuals and those with diverse abilities.</p>
 
-      🤝  Connecting Hearts and Minds: Communication is the heartbeat of human connection. Whether you communicate through text, voice, or video, UnitySpeak ensures that every interaction is accessible and enjoyable for everyone involved. Our user-friendly interface caters to various communication styles, ensuring a truly inclusive experience. <br /><br />
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🌐  A Platform for All:</span> At UnitySpeak, inclusivity is at the core of our mission. Our app provides a platform where individuals of all abilities can engage in meaningful conversations, share experiences, and build connections that transcend limitations.</p>
 
-      🎙️  Voice of Empowerment: For those who face speech challenges, UnitySpeak empowers them with innovative speech-to-text features, making their voices heard effortlessly. Additionally, our app facilitates easy communication through customizable symbols and images, ensuring that no message is left unexpressed. <br /><br />
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🤝  Connecting Hearts and Minds:</span> Communication is the heartbeat of human connection. Whether you communicate through text, voice, or video, UnitySpeak ensures that every interaction is accessible and enjoyable for everyone involved. Our user-friendly interface caters to various communication styles, ensuring a truly inclusive experience.</p>
 
-      🌈  Diverse Abilities, United Conversations: Diversity is our strength, and UnitySpeak celebrates the richness of human experience. With features designed to accommodate various abilities, our app becomes a powerful tool for fostering empathy, understanding, and unity among people from all walks of life. <br /><br />
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🎙️  Voice of Empowerment:</span> For those who face speech challenges, UnitySpeak empowers them with innovative speech-to-text features, making their voices heard effortlessly. Additionally, our app facilitates easy communication through customizable symbols and images, ensuring that no message is left unexpressed.</p>
 
-      🚀  Seamless Integration: UnitySpeak seamlessly integrates into your daily life, offering a wide range of features that adapt to your unique communication needs. From interactive chat rooms to personalized communication boards, we are committed to providing a platform that evolves with you. <br /><br />
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🌈  Diverse Abilities, United Conversations:</span> Diversity is our strength, and UnitySpeak celebrates the richness of human experience. With features designed to accommodate various abilities, our app becomes a powerful tool for fostering empathy, understanding, and unity among people from all walks of life.</p>
 
-      Join us on this journey of breaking down communication barriers, one conversation at a time. Download UnitySpeak today and be part of a community where everyone's voice is not just heard but celebrated. Together, let's create a world where communication knows no limits. <br /><br />
+      <p className="text-justify text-base"><span className="text-lg font-semibold">🚀  Seamless Integration:</span> UnitySpeak seamlessly integrates into your daily life, offering a wide range of features that adapt to your unique communication needs. From interactive chat rooms to personalized communication boards, we are committed to providing a platform that evolves with you.</p>
+
+      <p className="text-justify text-base">Join us on this journey of breaking down communication barriers, one conversation at a time. Download UnitySpeak today and be part of a community where everyone's voice is not just heard but celebrated. Together, let's create a world where communication knows no limits.</p>
+
     </div>
   );
 }
